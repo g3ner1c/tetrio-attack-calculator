2911f385aa41d1787eddc5cc375db2ea1c3ca829
awesomeplaya211 <74469257+awesomeplaya211@users.noreply.github.com>
2021-11-04 13:54:56 -0400

    + Combo and B2B and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# tetrio-attack-calculator
- Tetrio attack calculator
+# TETR.IO Attack Calculator
+
+## A fan made attack calculator for the TETR.IO garbage system
+
+Not affiliated with @o5k (osk) or TETR.IO
+
+I learnt JavaScript just to make this so sorry if code is bad lol
+
+### Accuracy
+
+I extrapolated the formulas for the garbage based off of osk's attack table:
+
+![osk's attack table](https://cdn.discordapp.com/attachments/674421736162197515/716081165886423110/2020-05-30_02-07-18.png)
+
+#### Singles
+
+[According to osk](https://discord.com/channels/673303546107658242/674421736162197515/787782034013159424) the formula for singles is
+
+```js
+Math.floor(Math.log1p(combo_level * 1.25))
+```
+
+#### General Attack Formula
+
+Based off of the attack table, I extrapolated
+
+```js
+Math.floor(combo_level/(4/(B2B_level + base_attack))) + B2B_level + base_attack
+```
+
+#### B2B Levels
+
+I couldn't find a general formula for the B2B chaining formula so i just went with [the one that osk said in discord](https://discord.com/channels/673303546107658242/674421736162197515/713419086486437960):
+
+| Level | B2Bs     |
+| ----- | -------- |
+| 0     | 0        |
+| 1     | 1-2      |
+| 2     | 3-7      |
+| 3     | 8-23     |
+| 4     | 24-66    |
+| 5     | 67-184   |
+| 6     | 185-503  |
+| 7     | 504-1369 |
+| 8     | 1370+    |
+
+Technically we don't actually know when level 8 ends but someones probably not going to get 1370+ B2Bs in a row anyways
+
+### Issues and Suggestions
+
+If you have any bug reports or suggestions [open an issue](https://github.com/awesomeplaya211/tetrio-attack-calculator/issues/new/choose)
--- script.js
+++ script.js
@@ -1,52 +1,180 @@
-var selected;
+var attack;
+var attack_list = [];
+
+var comboing = true;
+document.getElementById("toggle_combo").innerText = "Comboing On"
+var B2Bing = true
+document.getElementById("toggle_B2B").innerText = "B2B On"
+
+var combo_level = -1;
+var B2B_count = -1;
+var B2B_level = 0;
+var attack_count = 0;
+var total_lines = 0;
+
+var attackHTML = "<tr> <th>#</th> <th>Attack Type</th> <th>Lines Sent</th> <th>Combo</th> <th>B2B Count</th> <th>B2B Level</th> </tr>";
+document.getElementById("AttackList").innerHTML = attackHTML;
 
 var attack_dict = {
 
-    // "attackid": ["Name", Base Attack Count, B2B-able Flag]
+    // "attackid": ["Name", Base Attack, B2B-able Flag]
+    
 
-    "single": ["Single", 0, false],
-    "double": ["Double", 1, false],
-    "triple": ["Triple", 2, false],
-    "quad": ["Quad", 4, true],
-    "tsms": ["T-Spin Mini Single", 0, true],
-    "tss": ["T-Spin Single", 2, true],
-    "tsmd": ["T-Spin Mini Double", 1, true],
-    "tsd": ["T-Spin Double", 4, true],
-    "tst": ["T-Spin Triple", 6, true]
+    "single": ["Single", 0, false],          // attackFunc(combo_level, 0, 0)
+    "double": ["Double", 1, false],          // attackFunc(combo_level, 1, 0)
+    "triple": ["Triple", 2, false],          // attackFunc(combo_level, 2, 0)
+    "quad": ["Quad", 4, true],               // attackFunc(combo_level, 4, B2B_level)
+    "tsms": ["T-Spin Mini Single", 0, true], // attackFunc(combo_level, 0, B2B_level)
+    "tss": ["T-Spin Single", 2, true],       // attackFunc(combo_level, 2, B2B_level)
+    "tsmd": ["T-Spin Mini Double", 1, true], // attackFunc(combo_level, 1, B2B_level)
+    "tsd": ["T-Spin Double", 4, true],       // attackFunc(combo_level, 4, B2B_level)
+    "tst": ["T-Spin Triple", 6, true]        // attackFunc(combo_level, 6, B2B_level)
 
 };
 
-var attack_list = [];
 
-document.getElementById("button_add").onclick = function() {
-    
-    selected = document.getElementById("attack").value;
-    attack_list.push(selected);
+function attackFunc(combo_level, base_attack, B2B_level) {
 
-    console.log(selected);
-    console.log(attack_list);
+    if (B2B_level + base_attack == 0) {
 
-    // document.getElementById("selected").innerHTML = attack_dict[selected];
+        return Math.floor(Math.log1p(combo_level * 1.25));
 
-    var attackHTML = "<tr> <th>#</th> <th>Attack Type</th> <th>Lines Sent</th> </tr>";
+    } else {
 
-    var attack_count = 0;
-
-    var total_lines = 0;
-
-    for (var attack of attack_list) {
-
-        attack_count++;
-
-        total_lines += attack_dict[attack][1];
-
-        attackHTML += "<tr> <td>" + attack_count + "</td> <td>" + attack_dict[attack][0] + "</td> <td>" + attack_dict[attack][1] + "</td> </tr>";
+        return Math.floor(combo_level/(4/(B2B_level + base_attack))) + B2B_level + base_attack;
 
     }
 
-    attackHTML += "<tr> <td></td> <td> <b>Total:</b> </td> <td> <b>" + total_lines + "</b> </td> </tr>";
+}
 
-    document.getElementById("AttackList").innerHTML = attackHTML;
+
+document.getElementById("button_add").onclick = function() {
+    
+    attack = document.getElementById("attack").value;
+
+    attack_count++;
+
+    if (comboing) {
+
+        combo_level++;
+
+    } else {
+
+        combo_level = 0;
+
+    }
+
+    if (attack_dict[attack][2] === true && B2Bing) {
+
+        B2B_count++;
+
+        if (B2B_count <= 0) {
+            B2B_level = 0;
+        } else if (B2B_count <= 2) {
+            B2B_level = 1;
+        } else if (B2B_count <= 7) {
+            B2B_level = 2;
+        } else if (B2B_count <= 23) {
+            B2B_level = 3;
+        } else if (B2B_count <= 66) {
+            B2B_level = 4;
+        } else if (B2B_count <= 184) {
+            B2B_level = 5;
+        } else if (B2B_count <= 503) {
+            B2B_level = 6;
+        } else if (B2B_count <= 1369) {
+            B2B_level = 7;
+        } else {
+            B2B_level = 8;
+        }
+
+    } else {
+
+        if (attack_dict[attack][2]) {
+            B2B_count = 0;
+        } else {
+            B2B_count = -1;            
+        }
+
+        B2B_level = 0;
+
+    }
+
+    attack_list.push({
+        
+        "num": attack_count,
+        "id": attack,
+        "name": attack_dict[attack][0],
+        "combo": combo_level,
+        "B2B_count": B2B_count,
+        "B2B_level": B2B_level,
+        "lines_sent": attackFunc(combo_level, attack_dict[attack][1], B2B_level)
+    
+    });
+
+    console.log(attack);
+    console.log(attack_list);
+
+    if (attack_list.length === 1) {
+        
+        attackHTML = "<tr> <th>#</th> <th>Attack Type</th> <th>Lines Sent</th> <th>Combo</th> <th>B2B Count</th> <th>B2B Level</th> </tr>";
+
+    }
+
+    attackHTML += ("<tr> <td>" + attack_list[attack_count - 1]["num"] +
+                  "</td> <td>" + attack_list[attack_count - 1]["name"] +
+                  "</td> <td>" + attack_list[attack_count - 1]["lines_sent"] +
+                  "</td> <td>" + attack_list[attack_count - 1]["combo"] +
+                  "</td> <td>" + attack_list[attack_count - 1]["B2B_count"] +
+                  "</td> <td>" + attack_list[attack_count - 1]["B2B_level"] +
+                  "</td> </tr>");
+
+    // console.log(attackHTML);
+
+    total_lines += attack_list[attack_count - 1]["lines_sent"];
+
+    document.getElementById("AttackList").innerHTML = attackHTML + "<tr> <td></td> <td> <b>Total:</b> </td> <td> <b>" + total_lines + "</b> </td> </tr>";
+
+}
+
+
+document.getElementById("toggle_combo").onclick = function() {
+
+    if (comboing) {
+
+        comboing = false;
+        combo_level = 0;
+
+        document.getElementById("toggle_combo").innerText = "Comboing Off"
+
+    } else {
+
+        comboing = true;
+
+        document.getElementById("toggle_combo").innerText = "Comboing On"
+
+    }
+
+}
+
+
+document.getElementById("toggle_B2B").onclick = function() {
+
+    if (B2Bing) {
+
+        B2Bing = false;
+        B2B_count = -1;
+        B2B_level = 0;
+
+        document.getElementById("toggle_B2B").innerText = "B2B Off"
+
+    } else {
+
+        B2Bing = true;
+
+        document.getElementById("toggle_B2B").innerText = "B2B On"
+
+    }
 
 }
 
@@ -55,7 +183,19 @@ document.getElementById("button_clear").onclick = function() {
 
     attack_list = [];
 
-    document.getElementById("AttackList").innerHTML = ""
+    combo_level = -1;
+
+    B2B_count = -1;
+
+    B2B_level = 0;
+
+    attack_count = 0;
+
+    total_lines = 0;
+
+    attackHTML = "<tr> <th>#</th> <th>Attack Type</th> <th>Lines Sent</th> </tr>";
+
+    document.getElementById("AttackList").innerHTML = attackHTML;
 
 }
 
@@ -68,10 +208,10 @@ document.getElementById("button_image").onclick = function() {
 
       chart.style.display = "block";
 
-    }
+    } else {
 
-    else {
       chart.style.display = "none";
+
     }
      
 }
